2f6ff652748a8f9b2fbd389d82da625bc92b3982
V-Naga-Bhavesh <nagabhaveshvulli2007@gmail.com>
2026-07-18 12:08:54 +0530

    format changes again

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,22 @@
-// alert("Behold user!")
-console.log("Hello user!")
+var users=[
+    {
+        "name":"John Doe",
+        "gender":"Male",
+        "img":"john.png"
+    },
+    {
+        "name":"Jane Doe",
+        "gender":"Female",
+        "img":"jane.png"
+    }
+]
+var id=0;
+function toggleUser(){
+    id=(id+1)%2;
+    var userImage=document.getElementById("image");
+    userImage.src=users[id].img;
+    var userName=document.getElementById("name");
+    userName.innerHTML=users[id].name;
+    var userGender=document.getElementById("gender");
+    userGender.innerHTML=users[id].gender;
+}
